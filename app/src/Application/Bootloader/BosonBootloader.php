151fd20c8ffd363031cda3c3651c94cfdb868842
roxblnfk <roxblnfk@ya.ru>
2025-07-13 12:16:13 +0400

    Init CTX

--- app/src/Application/Bootloader/BosonBootloader.php
+++ app/src/Application/Bootloader/BosonBootloader.php
@@ -9,17 +9,8 @@ use Boson\ApplicationCreateInfo;
 use Boson\Dispatcher\EventListenerProviderInterface;
 use Boson\WebView\WebViewCreateInfo;
 use Boson\Window\WindowCreateInfo;
-use Monolog\Level;
 use Spiral\Boot\Bootloader\Bootloader;
-use Spiral\Http\Middleware\ErrorHandlerMiddleware;
-use Spiral\Monolog\Bootloader\MonologBootloader;
-use Spiral\Monolog\Config\MonologConfig;
 
-/**
- * The bootloader is responsible for configuring the application specific loggers.
- *
- * @link https://spiral.dev/docs/basics-logging
- */
 final class BosonBootloader extends Bootloader
 {
     public function defineSingletons(): array
@@ -28,36 +19,6 @@ final class BosonBootloader extends Bootloader
             EventListenerProviderInterface::class => Application::class,
             Application::class => $this->createApplication(),
         ];
-    }
-
-    public function init(MonologBootloader $monolog): void
-    {
-        // HTTP level errors
-        $monolog->addHandler(
-            channel: ErrorHandlerMiddleware::class,
-            handler: $monolog->logRotate(
-                directory('runtime') . 'logs/http.log',
-            ),
-        );
-
-        // app level errors
-        $monolog->addHandler(
-            channel: MonologConfig::DEFAULT_CHANNEL,
-            handler: $monolog->logRotate(
-                filename: directory('runtime') . 'logs/error.log',
-                level: Level::Error,
-                maxFiles: 25,
-                bubble: false,
-            ),
-        );
-
-        // debug and info messages via global LoggerInterface
-        $monolog->addHandler(
-            channel: MonologConfig::DEFAULT_CHANNEL,
-            handler: $monolog->logRotate(
-                filename: directory('runtime') . 'logs/debug.log',
-            ),
-        );
     }
 
     private function createApplication(): Application
